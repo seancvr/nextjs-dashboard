9b9e0bd15674be78b8c6069c5589f7e940627303
seancvr <134600598+seancvr@users.noreply.github.com>
2024-11-26 15:48:02 -0700

    Add comments to authentication

--- auth.config.ts
+++ auth.config.ts
@@ -3,11 +3,15 @@ import type { NextAuthConfig } from 'next-auth';
 // authConfig object
 export const authConfig = {
   pages: {
-    signIn: '/login',
+    signIn: '/login', // NextAuth will direct user to /login instead of NextAuth default page
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      // auth propery contains users session
+      // request property contains incoming request
       const isLoggedIn = !!auth?.user;
+
+      // check all dashboard routes
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
@@ -18,5 +22,6 @@ export const authConfig = {
       return true;
     },
   },
+  // providers options lists login options
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth';
+// import authConfig object into middleware
 import { authConfig } from './auth.config';
 
 // Initialize NextAuth with the authConfig object
@@ -6,5 +7,13 @@ export default NextAuth(authConfig).auth;
  
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  // matcher allows you to filter middleware to run on specific paths
+  /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - .png files
+     */
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
